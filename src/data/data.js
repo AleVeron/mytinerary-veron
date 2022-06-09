4eec62985d4cab621492266a60be616a86afd348
AleVeron <alejoveron65@gmail.com>
2022-06-09 14:33:55 -0300

    9/06

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,5 @@
 const cities = [
-    {id: "1", name: "Tokyo", image: "https://thumbs.dreamstime.com/b/tokio-jap%C3%B3n-36162918.jpg"},
+    {id: "1", name: "Tokyo", image: "https://www.gotokyo.org/es/plan/tokyo-outline/images/main.jpg"},
     {id: "2", name: "Sidney", image: "https://media.iatiseguros.com/wp-content/uploads/2019/04/04011211/que-hacer-sidney-6.jpg"},
     {id: "3", name: "Shanghai", image: "https://www.riotgames.com/darkroom/1440/94c6153012aa766d7aaf6bb6b35dd35a:52617d5f2fda6d0b4a9669146c047e24/shanghai-adobestock-300255822.png"},
     {id: "4", name: "Roma", image: "https://historia.nationalgeographic.com.es/medio/2019/12/11/coliseo-roma_2924b6ae_1280x720.jpg"},
@@ -7,7 +7,7 @@ const cities = [
     {id: "6", name: "Machu Pichu", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Machu_Picchu%2C_Peru.jpg/1200px-Machu_Picchu%2C_Peru.jpg"},
     {id: "7", name: "Mar del Plata", image: "https://imgs-akamai.mnstatic.com/7f/72/7f7293391fc41ff0aa6324cbee7c48f1.jpg"},
     {id: "8", name: "Madrid", image: "https://i0.wp.com/sinmordaza.com/wp-content/uploads/2020/09/portada-madrid.jpg?fit=1080%2C725&ssl=1"},
-    {id: "9", name: "Londres", image: "https://elviajerofeliz.com/wp-content/uploads/2017/10/Rutas-por-Londres.jpg"},
+    {id: "9", name: "London", image: "https://elviajerofeliz.com/wp-content/uploads/2017/10/Rutas-por-Londres.jpg"},
     {id: "10", name: "Hong Kong", image: "https://www.mendozapost.com/files/image/259/259256/5ec7a8e1b4477.jpg"},
     {id: "11", name: "Bogota", image: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/JAAYCWLOQRHOTKSLAZAH37REYM.jpeg"},
     {id: "12", name: "Berlin", image: "https://www.visitberlin.de/system/files/image/og_image.jpg"}
