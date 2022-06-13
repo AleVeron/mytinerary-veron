96b3bba5e56ef4e8b807eeac6c9424c7667123cc
AleVeron <alejoveron65@gmail.com>
2022-06-13 18:32:02 -0300

    cards filter

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -1,26 +1,20 @@
 import "./cards.css";
-import cities from '../../data/data.js';
 
 
-const Cards = () => {
+
+const Cards = ({city}) => {
     
     return (
 
-        <>
-        
-        {cities.map((city)=>(
-            
-                <div key={city.id} className="container card mb-3 col-11 col-sm-12 col-md-5">
-                <img src={city.image} className="card-img-top" alt={city.name}/>
-                <div className="card-body">
+        <>           
+            <div key={city.id} className="container card mb-3 col-11 col-sm-12 col-md-5">
+              <img src={city.image} className="card-img-top" alt={city.name}/>
+              <div className="card-body">
                   <h5 className="card-title">{city.name}</h5>
                   <p className="card-text">{city.description}</p>
                   <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
               </div>
-              
-        ))}
-        
+              </div>  
         </>
         
     )
--- frontend/src/pages/Cities/Cities.jsx
+++ frontend/src/pages/Cities/Cities.jsx
@@ -1,18 +1,40 @@
 import "./cities.css";
 import Cards from "../../components/Cards/Cards";
+import cities from '../../data/data.js';
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Cities (){
 
+    const [city, setCity] = useState([])
+    const [input, setInput] = useState('')
 
+    useEffect(()=>{
+        setCity(cities)
+        let city = cities.filter(c => c.name.toLowerCase().startsWith(input.trim().toLocaleLowerCase()))
+        setCity(city)
+    },[input])
 
 
     return(
         <div className="cities d-flex flex-column justify-content-center">
 
             <h1>Welcome</h1>
-            <input className="mb-4 search container" type="search"></input>
+
+            <input 
+            className="mb-4 search container" 
+            type="search" 
+            placeholder="Search here"
+            onKeyUp={(e)=>{
+                setInput(e.target.value)
+            }}
+            >
+            </input>
+
             <div className="d-flex flex-md-row flex-wrap gap-3 container">
-            <Cards/>
+
+            {city.length > 0 ? city.map(city => <Cards city={city} key={city.id}/>) : <div className="error"><h1>Not found city</h1></div>}
+
             </div>
 
         </div>
